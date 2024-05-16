cde0407eae6749d92fdf5d1e69067c6c7aab05f5
saqib ullah <22pwbcs0916@uetpeshawar.edu.pk>
2024-05-16 09:29:09 +0500

    directory structure again solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import './index.css'
-import Home from './Pages/Home/HomePage'
-import MiniDrawer from './Pages/Home/Components/Dashboard/MiniDrawer'
+import Home from "./Pages/Home/HomePage"
+import MiniDrawer from "../src/Pages/Dashboard/MiniDrawer"
 import {BrowserRouter as Router ,Route,Routes} from 'react-router-dom'
-import ContactForm from './Pages/ContactForm/ContactForm'
-import LoginUser from './Pages/UserRegistration/LoginUser'
-import RegisterUser from './Pages/UserRegistration/RegisterUser'
+
 
 
 function App() {
