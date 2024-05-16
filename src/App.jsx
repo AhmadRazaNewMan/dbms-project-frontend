77f02e141a4336fa4890ed3bd878a6565b105456
saqib ullah <22pwbcs0916@uetpeshawar.edu.pk>
2024-05-16 08:35:04 +0500

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './index.css'
 import Home from './Pages/Home/HomePage'
+import MiniDrawer from './Pages/Home/Components/Dashboard/MiniDrawer'
+import {BrowserRouter as Router ,Route,Routes} from 'react-router-dom'
 
 
 function App() {
  
   return (
-    <>
-     <Home/>
-   
-     
-    </>
+   <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='dashboard' element={<MiniDrawer/>}/>
+      </Routes>
+   </Router>
   )
 }
 
