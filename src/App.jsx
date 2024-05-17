9b70713520d3bb271edc8c2c0a373fe1408fe89f
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-05-17 15:52:04 +0500

    adding notification setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,29 @@
-import React from 'react'
-import './index.css'
+import React from 'react';
+import './index.css';
 
-import Home from "./Pages/Home/HomePage"
-import MiniDrawer from "../src/Pages/Dashboard/MiniDrawer"
-import {BrowserRouter as Router ,Route,Routes} from 'react-router-dom'
-import RegistractionPage from './Pages/UserRegistration/RegisterUser'
-import LoginUser from './Pages/UserRegistration/LoginUser'
-import AdminNotification from './Pages/NotificationSection/AdminNotification'
-
-
-
+import Home from "./Pages/Home/HomePage";
+import MiniDrawer from "../src/Pages/Dashboard/MiniDrawer";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import RegistractionPage from './Pages/UserRegistration/RegisterUser'
+// import LoginUser from './Pages/UserRegistration/LoginUser'
+// import AdminNotification from './Pages/NotificationSection/AdminNotification'
+import NotificationSlider from './Pages/NotificationSection/NotificationSlider';
+import SingleNotificationPage from './Pages/NotificationSection/SingleNotificationPage';
 
 function App() {
- 
   return (
-<>
-   {/* <Router> */}
-      {/* <Routes> */}
-        {/* <Route path='/' element={<Home/>}/> */}
+    <Router>
+      <Routes>
+        <Route path='/' element={<NotificationSlider />} />
+        <Route path='notification' element={<SingleNotificationPage />} />
         {/* <Route path='dashboard' element={<MiniDrawer/>}/> */}
         {/* <Route path='register' element={<register/>}/> */}
-      {/* </Routes> */}
-   {/* </Router> */}
-   {/* <RegistractionPage/> */}
-   {/* <LoginUser/> */}
-   <AdminNotification/>
-   
-
-
-   </>
-   
-     
-    
-
-
-  )
+      </Routes>
+    </Router>
+    // <RegistractionPage/>
+    // <LoginUser/>
+    // <AdminNotification/>
+  );
 }
 
-export default App
+export default App;
