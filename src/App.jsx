8471586948bdd491b1b400996f4899b1cc5d1635
saqib ullah <22pwbcs0916@uetpeshawar.edu.pk>
2024-05-17 17:40:13 +0500

    card added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import Home from "./Pages/Home/HomePage"
 import MiniDrawer from "../src/Pages/Dashboard/MiniDrawer"
 import {BrowserRouter as Router ,Route,Routes} from 'react-router-dom'
+import RecipeReviewCard from './Pages/CardsComponent/RecipeReviewCard'
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='dashboard' element={<MiniDrawer/>}/>
+        <Route path='card' element={<RecipeReviewCard/>}/>
       </Routes>
    </Router>
 
